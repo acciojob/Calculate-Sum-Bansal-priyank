a76471d272b52b6a4f98fb6684f380594baf5cbe
Priyank Bansal <165632145+Bansal-priyank@users.noreply.github.com>
2025-08-13 09:14:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById("sum_btn").addEventListener("click", function() {
+	let num1 = document.getElementById("n1").value;
+	let num2 = document.getElementById("n2").value;
+
+	if(num1 !== "" && num2 !== ""){
+		let sum = Number(num1) + Number(num2);
+		document.getElementById("sum").innertext = sum;
+	} else {
+		document.getElementById("sum").innertext = "";
+	}
+});
